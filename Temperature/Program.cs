88e965d3cf80158c0d2fb28d68c09c6f4c0f2d8c
oliverchen415 <oliverchensf@outlook.com>
2021-03-04 21:57:47 -0800

    Add project files.

---
+++ Temperature/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using TemperatureLibrary;
+
+namespace TemperatureProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            
+            Console.WriteLine("Temperature conversion\r");
+            Console.WriteLine("----------------------\n");
+
+            while (!endApp)
+            {
+                string temp = "";
+                double result = 0;
+
+                Console.WriteLine("Type a temperature, then press enter.");
+                temp = Console.ReadLine();
+
+                double cleanNum = 0;
+                while (!double.TryParse(temp, out cleanNum))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number: ");
+                    temp = Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an option (convert from - convert to)");
+                Console.WriteLine("\ta) F to C");
+                Console.WriteLine("\tb) F to K");
+                Console.WriteLine("\tc) C to F");
+                Console.WriteLine("\td) C to K");
+                Console.WriteLine("\te) K to F");
+                Console.WriteLine("\tf) K to C");
+                Console.Write("Your Option? ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = TempConvert.Conversion(cleanNum, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("The operation will result in an invalid value.\n");
+                    }
+                    else Console.WriteLine($"Your result: {result:F2}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception has occurred! " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Press 'n' and 'Enter' to exit the app, or any other key and 'Enter' to continue: ");
+
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
+
+            }
+            return;
+           
+        }
+
+    }
+}
+
+
---
+++ TemperatureLibrary/TemperatureLibrary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TemperatureLibrary
+{
+    public class TempConvert
+    {
+        public static double Conversion(double temp, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = (temp - 32.0) * (5.0 / 9.0);
+                    break;
+                case "b":
+                    result = (temp - 32.0) * (5.0 / 9.0) + 273.15;
+                    break;
+                case "c":
+                    result = (temp * 5.0 / 9.0) + 32.0;
+                    break;
+                case "d":
+                    result = temp + 273.15;
+                    break;
+                case "e":
+                    result = (temp - 273.15) * (9.0 / 5.0) + 32.0;
+                    break;
+                case "f":
+                    result = temp - 273.15;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+ }
+
